e3c44c483f94cf3794d5258eff38d8f1b0861a55
ajilty <github@ajilty.com>
2026-02-05 06:06:29 -0500

    Simplify direnv not found check

--- plugins/direnv/direnv.plugin.zsh
+++ plugins/direnv/direnv.plugin.zsh
@@ -1,8 +1,5 @@
-# If direnv is not found, don't continue and print a warning
-if (( ! $+commands[direnv] )); then
-  echo "Warning: direnv not found. Please install direnv and ensure it's in your PATH before using this plugin."
-  return
-fi
+# Don't continue if direnv is not found
+command -v direnv &>/dev/null || return
 
 _direnv_hook() {
   trap -- '' SIGINT;
